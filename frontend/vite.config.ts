d59e9c2b54649fbdd8fc2a2d8f49d330cd7fb66a
Bobby Tumur <bodikhuutumurbaatar@gmail.com>
2024-11-04 18:40:43 +0900

    Added build logic on NPM

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,4 +5,15 @@ import { defineConfig } from "vite"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), TanStackRouterVite()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.split('node_modules/')[1].split('/')[0].toString();
+          }
+        },
+      },
+    },
+  },
 })
