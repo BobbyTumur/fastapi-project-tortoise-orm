ea3042d10e3cc7ccf0247042d80608af9dab667e
Bobby Tumur <bodikhuutumurbaatar@gmail.com>
2025-01-18 12:19:08 +0900

    new model for file transferrer to fetch its info

--- backend/app/models/file_transfer.py
+++ backend/app/models/file_transfer.py
@@ -1,6 +1,8 @@
-from typing import Literal
-from pydantic import BaseModel
+import os
+from uuid import UUID
 from datetime import datetime
+from typing import Literal
+from pydantic import BaseModel, ConfigDict, field_validator
 
 class Token(BaseModel):
     access_token: str
@@ -11,7 +13,6 @@ class PromptUrl(BaseModel):
     expiry_hours: int
     type: Literal["download", "upload"]
     file_name: str | None = None
-
 
 class ResponseURL(BaseModel):
     url: str
@@ -28,3 +29,16 @@ class Validated(BaseModel):
 
 class DownloadUrl(BaseModel):
     url: str
+
+class TempUserPublic(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: UUID
+    company_name: str
+    file_name: str | None = None
+
+    @field_validator("file_name", mode="before")
+    def extract_file_name(cls, v):
+        if v is None:
+            return v
+        return os.path.basename(v)
