9e4f7f1b14154bae95e1b9b61fc28201aaae243f
Bobby Tumur <bodikhuutumurbaatar@gmail.com>
2025-01-25 17:43:18 +0900

    Test email to write to Log database

--- backend/app/api/routes/utils.py
+++ backend/app/api/routes/utils.py
@@ -1,7 +1,9 @@
+from datetime import datetime
 from fastapi import APIRouter, Depends
 from pydantic.networks import EmailStr
 
-from app.api.dep import get_current_active_superuser
+from app import crud
+from app.api.dep import get_current_active_superuser, CurrentUser
 from app.models.general_models import Message
 from app.utils import generate_test_email, send_email
 
@@ -12,15 +14,28 @@ router = APIRouter(prefix="/utils", tags=["utils"])
     "/test-email",
     dependencies=[Depends(get_current_active_superuser)]
 )
-async def test_email(email_to: EmailStr) -> Message:
+async def test_email(email_to: EmailStr, current_user: CurrentUser) -> Message:
     """
     Test emails.
     """
+    start = datetime.now()
+
     email_data = generate_test_email(email_to=email_to)
-    await send_email(
+    response = await send_email(
         email_to=email_to,
         subject=email_data.subject,
         html_content=email_data.html_content,
+    )
+    log_data = {
+        "start_time": start,
+        "end_time": datetime.now(),
+        "is_ok": True if response else False,
+        "content": f"{response}, {current_user.username}",
+    }
+    await crud.add_log_entry(
+        service_name="Sendgrid",
+        service_sub_name="Mail API v3",
+        log_data=log_data,
     )
     return Message(message="Test email sent")
 
