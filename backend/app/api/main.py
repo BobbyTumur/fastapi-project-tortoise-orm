ca7d7517e7c339b5a45f5f8c7bdb4735aa2a5eb3
Bobby Tumur <bodikhuutumurbaatar@gmail.com>
2025-01-25 17:42:47 +0900

    Firebase route added

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from app.api.routes import login, utils, services, users, totp, websocket, user_services, file_transfer 
+from app.api.routes import login, utils, services, users, totp, websocket, user_services, file_transfer, firebase
 
 api_router = APIRouter()
 api_router.include_router(login.router)
@@ -9,5 +9,6 @@ api_router.include_router(users.router)
 api_router.include_router(user_services.router)
 api_router.include_router(services.router)
 api_router.include_router(websocket.router)
-api_router.include_router(utils.router)
 api_router.include_router(file_transfer.router)
+api_router.include_router(firebase.router)
+api_router.include_router(utils.router)
